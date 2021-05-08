45bdfd258ad04b77222a84e1c6afb4ffa9528921
mrwhite1004 <353349591@qq.com>
2021-05-08 23:23:20 +0800

    6

--- app/src/main/java/com/mini_tiktok/bytedance_camp_project/service/VideoService.java
+++ app/src/main/java/com/mini_tiktok/bytedance_camp_project/service/VideoService.java
@@ -4,24 +4,10 @@ import com.google.gson.JsonObject;
 
 import java.util.List;
 
-import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Multipart;
-import retrofit2.http.POST;
-import retrofit2.http.Part;
-import retrofit2.http.Query;
 
 public interface VideoService {
-
-    @Multipart
-    @POST("video")
-    Call<com.mini_tiktok.bytedance_camp_project.activity.model.UploadResponse> submitVideo(@Query("student_id") String studentId,
-                                     @Query("user_name") String userName,
-                                     @Query("extra_value") String extraValue,
-                                     @Part MultipartBody.Part coverImage,
-                                     @Part MultipartBody.Part video);
-
     @GET("video")
     Call<JsonObject> getVideoInfo();
 }
